a7daee92a5f5f9b94b330c2ff52384b8e5c04c7b
Harshal0902 <harshalraikwar07@gmail.com>
2022-07-20 02:23:27 +0530

    ✨ Add spotlight and ambientlight

--- pages/index.js
+++ pages/index.js
@@ -5,11 +5,13 @@ import { Environment, OrbitControls } from "@react-three/drei"
 import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader"
 
 const Model = () => {
+  // location of the 3D model
   const gltf = useLoader(GLTFLoader, "/earth/scene.gltf");
   return (
-    <div>
+    <>
+      {/* Use scale to control the size of the 3D model */}
       <primitive object={gltf.scene} scale={0.01} />
-    </div>
+    </>
   );
 };
 
@@ -24,10 +26,14 @@ export default function Home() {
 
       <div className="globe">
         <Canvas shadows dpr={[1, 2]} camera={{ position: [0, 0, 4], fov: 50 }}>
+          <ambientLight intensity={0.7} />
+          <spotLight intensity={0.5} angle={0.1} penumbra={1} position={[10, 15, 10]} castShadow />
           <Suspense fallback={null}>
             <Model />
+            {/* To add environment effect to the model */}
             <Environment preset="city" />
           </Suspense>
+          <OrbitControls autoRotate />
         </Canvas>
       </div>
 
