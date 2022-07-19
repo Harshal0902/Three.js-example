81f01b0074a082835daa00e387e682bb177a0853
Harshal0902 <harshalraikwar07@gmail.com>
2022-07-20 02:03:06 +0530

    💡 Adding comments

--- pages/index.js
+++ pages/index.js
@@ -4,14 +4,16 @@ import { Canvas, useLoader } from "@react-three/fiber"
 import { Environment, OrbitControls } from "@react-three/drei"
 import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader"
 
-function Model() {
-  const gltf = useLoader(GLTFLoader, '/earth/scene.gltf');
+const Model = () => {
+  // location of the 3D model
+  const gltf = useLoader(GLTFLoader, "/earth/scene.gltf");
   return (
-    <>
+    <div>
+      {/* Use scale to control the size of the 3D model */}
       <primitive object={gltf.scene} scale={0.01} />
-    </>
+    </div>
   );
-}
+};
 
 export default function Home() {
   return (
@@ -24,16 +26,12 @@ export default function Home() {
 
       <div className="globe">
         <Canvas shadows dpr={[1, 2]} camera={{ position: [0, 0, 4], fov: 50 }}>
-          <ambientLight intensity={0.7} />
-          <spotLight intensity={0.5} angle={0.1} penumbra={1} position={[10, 15, 10]} castShadow />
           <Suspense fallback={null}>
-            <Model  />
+            <Model />
             <Environment preset="city" />
           </Suspense>
-          <OrbitControls autoRotate/>
         </Canvas>
       </div>
-
 
     </div>
   )
