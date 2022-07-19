f4714522edbf56a6276e5f7ba949a1da7c2d05c9
Harshal0902 <harshalraikwar07@gmail.com>
2022-07-20 02:07:42 +0530

    💡 Remove comments

--- pages/index.js
+++ pages/index.js
@@ -5,11 +5,9 @@ import { Environment, OrbitControls } from "@react-three/drei"
 import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader"
 
 const Model = () => {
-  // location of the 3D model
   const gltf = useLoader(GLTFLoader, "/earth/scene.gltf");
   return (
     <div>
-      {/* Use scale to control the size of the 3D model */}
       <primitive object={gltf.scene} scale={0.01} />
     </div>
   );
